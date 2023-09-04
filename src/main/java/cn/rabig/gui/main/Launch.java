82f36cb5afab13267267a5abb38929cb7cea849c
IDeLoveYou <rabig@foxmail.com>
2023-09-04 19:56:03 +0800

    fix pom

--- src/main/java/cn/rabig/gui/main/Launch.java
+++ src/main/java/cn/rabig/gui/main/Launch.java
@@ -10,7 +10,7 @@ public class Launch {
     /**
      * javafx应用程序需要添加额外参数指定javafx-sdk路径才能运行
      * 于是我们通过非继承Application类的的方法来调用launch()方法
-     * 当然也可以通过javafx:run指令调用maven插件来运行
+     * 当然也可以通过javafx:run指令调用maven插件来运行，但请添加对应的插件
      * 由于这种方式启动会创建一个匿名内部类，因此会报一个警报，但不影响程序运行
      *
      * @param args [java.lang.String]
